be5d03aa168b34e328c19fcba67d4cad5ea75007
VIP\bhyang <wa@q.com>
2018-03-02 18:11:08 +0800

    更新到save模块，但是save存在问题

--- application/controllers/admin/BlogC.php
+++ application/controllers/admin/BlogC.php
@@ -8,7 +8,8 @@
 
 class BlogC extends CI_Controller
 {
-    public function add() {
+    public function add()
+    {
         /**-------先定义表单验证规则------*/
         //导入表单验证类
         $this->load->library('form_validation');
@@ -31,11 +32,54 @@ class BlogC extends CI_Controller
             redirect(site_url('admin/blogc/lst'));
         }
     }
+
+    /**
+     * 日志列表
+     */
     public function lst()
     {
-        echo 'in lst';
+        $this->load->model('BlogM', 'bm');
+        $data = $this->bm->search(5);
+        //echo var_dump($data);
+        $this->load->view('admin/blogc/lst', $data);
+    }
+
+    /**
+     * 删除日志
+     */
+    public function delete($id)
+    {
+        $this->load->model('BlogM', 'bm');
+        $data = $this->bm->delete($id);
+        redirect(site_url('admin/blogc/lst'));
+    }
+
+    public function save($id=10)
+    {
+        //生成日志模型
         $this->load->model('BlogM');
-        $data = $this->BlogM->query();
-        echo var_dump($data);
+        //先取出要修改的记录的信息
+        $info = $this->BlogM->find($id);
+        //echo var_dump($info);die;
+
+        /**-------先定义表单验证规则------*/
+        //导入表单验证类
+        $this->load->library('form_validation');
+        //定义规则
+        $this->form_validation->set_rules('title', '标题', 'required|max_length[150]');
+        $this->form_validation->set_rules('content', '内容', 'required');
+
+        /**-------验证表单------*/
+        if ($this->form_validation->run() === false)
+        {
+            //如果验证失败，则显示表单，错误信息在表单中显示
+            $this->load->view('admin/blogc/save', $info);
+        }
+        else {
+
+            $this->BlogM->save();
+            //跳转
+            redirect(site_url('admin/blogc/lst'));
+        }
     }
 }
